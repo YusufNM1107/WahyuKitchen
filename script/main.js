23fd7785bb40ad5c7eab9c49d6db2cc2a2d7c142
YusufNM1107 <runruntemboro123@gmail.com>
2024-07-01 16:30:34 +0700

    First Commit

---
+++ script/main.js
@@ -0,0 +1,17 @@
+// Scroll dengan lambat
+
+$(document).ready(function(){
+    // Tambahkan selector untuk tombol yang ingin diaktifkan smooth scrolling
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      var target = this.hash;
+      var $target = $(target);
+  
+      // Animasi scroll
+      $('html, body').animate({
+        'scrollTop': $target.offset().top
+      }, 1000, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+  });
